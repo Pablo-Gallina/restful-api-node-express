5964e28f14769b048c4cd531d9f8ac735c6750e6
Pablo Gallina <pablogallinag@gmail.com>
2021-12-14 20:26:35 -0600

    Servidor web express (hola mundo)

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+const port = 3000;
+
+const resListen = ()=> console.log(`escuchando en el puerto http://localhost:${port}/`);
+// metodos
+// app.delete
+// app.put
+// app.post //etc
+
+// Levantar servidor
+// '/', ruta
+
+app.get('/', function(req, res) {
+    // tanto req como res, tienen varios metodos disponibles
+    //send (enviar informacion al servidor)
+    res.send('Hola mundo');
+});
+
+// En que puerto estará escuchando el servidor
+app.listen(port, resListen)
