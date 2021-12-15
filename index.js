a1032ba890551d1f1e0cf5071c6349052f3e6ab4
Pablo Gallina <pablogallinag@gmail.com>
2021-12-14 20:46:57 -0600

    Agregando otra ruta y configurando variable entorno

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-const port = 3000;
+//const port = 3000;
+//variables de entorno
+const port = process.env.PORT || 3000; //En las variables de entorno toma el PORT sino existe el valor será 3000
+//crear la variable de entorno en el cmd
+//windows: set PORT=5000
 
 const resListen = ()=> console.log(`escuchando en el puerto http://localhost:${port}/`);
 // metodos
@@ -17,6 +21,11 @@ app.get('/', function(req, res) {
     //send (enviar informacion al servidor)
     res.send('Hola mundo');
 });
+
+//otras rutas
+app.get('/api/usuarios', (req, res)=>{
+    res.send([{ 'Usuario':'Pablo' },{ 'Usuario':'Daniel'}, { 'Usuario':'Yostin' }])
+})
 
 // En que puerto estará escuchando el servidor
 app.listen(port, resListen)
