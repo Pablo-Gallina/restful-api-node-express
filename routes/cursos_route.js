08b0d58ad6266a110b2c1ce617dca0bb5d9666ca
Pablo Gallina <pablogallinag@gmail.com>
2022-01-04 16:08:42 -0600

    CRUD cursos finalizado

--- routes/cursos_route.js
+++ routes/cursos_route.js
@@ -1,17 +1,133 @@
 const express = require('express');
 const ruta = express.Router();
 
-// data
-const cursos = [
-    { id: 1, nombre: 'JS'},
-    { id: 2, nombre: 'CSS'},
-    { id: 3, nombre: 'HTML'},
-    { id: 4, nombre: 'REACT'}
-]
+//Modelos
+const Cursos = require('../models/cursos_model');
 
-ruta.get('/', (req, res)=>{
-    res.send(cursos)
+//Esquemas (schema)
+const schema = require('../schema/cursos_schema');
+
+// ****CRUD RUTAS
+//?POST
+ruta.post('/', (req, res)=>{
+    const { titulo, descripcion, imagen } = req.body;
+
+    // Validacion de datos por medio del modulo JOI (esquema)
+    const { error, value } = schema.validate({ titulo, descripcion, imagen });
+
+    // Si existe algun error, retornar el error
+    if (error) return res.status(404).send(error);
+
+    const resultado = crearCurso(value); // Creando el curso
+
+    // Verificar si el curso fue creado
+    resultado
+        .then( curso => res.json({curso: curso}))
+        .catch( e => res.status(400).json({
+            error: e
+        }));
 })
+
+//?GET
+ruta.get('/', (req, res)=>{
+    const curso = listarCursos();
+
+    curso
+        .then( curso => {res.json({curso})})
+        .catch( e => {res.status(400).json({
+            error: e
+        })});
+})
+
+//?PUT
+ruta.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    const { titulo, descripcion, imagen } = req.body;
+
+    // Validacion de datos por medio del modulo JOI (esquema)
+    const { error, value } = schema.validate({ titulo, descripcion, imagen });
+
+    // Si existe algun error, retornar el error
+    if (error) return res.status(404).send(error);
+
+    const resultado = editarCurso(id, value);
+    
+    resultado
+        .then( curso => {res.json({curso})})
+        .catch( e => {res.status(400).json({
+            error: e
+        })});
+})
+
+//?DELETE (desactivar usuario)
+ruta.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    const resultado = desactivarCurso(id);
+    
+    resultado
+        .then( curso => {res.json({curso})})
+        .catch( e => {res.status(400).json({
+            error: e
+        })});
+})
+
+
+//*****CRUD USUARIOS
+//?POST
+//Funcion para guardar dato (body, los datos enviados por el cliente)
+const crearCurso = async ({ titulo, descripcion, imagen }) =>{
+    // Creando el curso en base al modelo
+    const curso = new Cursos({ titulo, descripcion, imagen }) // los demas campos no son requeridos
+
+    try {
+        const res = await curso.save(); // Guardarlo en mongodb
+        return res;
+    } catch (e) {
+        throw e;
+    }
+}
+
+//?GET
+const listarCursos = async ()=>{
+    try {
+        const cursos = await Cursos.find({estado:true})
+        return cursos;
+    } catch (e) {
+        throw e;
+    }
+}
+
+//?PUT
+const editarCurso = async (id, { titulo, descripcion, imagen }) => {
+    try {
+        const res = await Cursos.findByIdAndUpdate({_id: id},{
+            $set: {
+                titulo,
+                descripcion,
+                imagen
+            }
+        }, { new:true }); 
+        return res;
+
+    } catch (e) {
+        throw e;
+    }
+}
+
+//?DELETE (desactivar curso)
+const desactivarCurso = async id => {
+    try {
+        const res = await Cursos.findByIdAndUpdate({_id: id},{
+            $set: {
+                estado: false
+            }
+        }, { new:true }); 
+        return res;
+
+    } catch (e) {
+        throw e;
+    }
+}
 
 //Exportar todas las rutar
 module.exports = ruta;
---
+++ schema/cursos_schema.js
@@ -0,0 +1,22 @@
+// Modulo para validacion de datos
+const Joi = require('joi');
+
+//****Esquemas de validacion (JOI)
+const schema = Joi.object({
+    titulo: Joi.string()
+        .min(3)
+        .max(50)
+        .required(),
+
+    descripcion: Joi.string()
+        .min(3)
+        .max(150)
+        .required(),
+
+    imagen: Joi.string()
+        .min(3)
+        .max(100)
+        .required(),
+})
+//Exportar schema
+module.exports = schema;
