1d98ad8c0c47d321c37972b92141eaf69248c08d
Pablo Gallina <pablogallinag@gmail.com>
2022-01-04 14:43:11 -0600

    PUT editar usuario

--- routes/usuarios_route.js
+++ routes/usuarios_route.js
@@ -4,16 +4,6 @@ const ruta = express.Router();
 //Modelos
 const Usuarios = require('../models/usuarios_model');
 
-ruta.get('/', (req, res)=>{
-    const usuarios = listarUsuarios();
-
-    usuarios
-        .then( usuarios => {res.json({usuarios: usuarios})})
-        .catch( e => {res.status(400).json({
-            error: e
-        })});
-})
-
 // ****CRUD RUTAS
 //?POST
 ruta.post('/', (req, res)=>{
@@ -27,6 +17,30 @@ ruta.post('/', (req, res)=>{
             error: e
         }));
 })
+//?GET
+ruta.get('/', (req, res)=>{
+    const usuarios = listarUsuarios();
+
+    usuarios
+        .then( usuarios => {res.json({usuarios: usuarios})})
+        .catch( e => {res.status(400).json({
+            error: e
+        })});
+})
+
+//?PUT
+ruta.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    const body = req.body;
+    const resultado = editarUsuario(id, body);
+    
+    resultado
+        .then( usuario => {res.json({usuario: usuario})})
+        .catch( e => {res.status(400).json({
+            error: e
+        })});
+})
+
 
 //*****CRUD USUARIOS
 //?POST
@@ -48,6 +62,23 @@ const listarUsuarios = async ()=>{
     try {
         const usuarios = await Usuarios.find()
         return usuarios;
+    } catch (e) {
+        throw e;
+    }
+}
+
+//?PUT
+const editarUsuario = async (id, {nombre, email, password}) => {
+    try {
+        const res = await Usuarios.findByIdAndUpdate({_id: id},{
+            $set: {
+                nombre,
+                email,
+                password
+            }
+        }, { new:true }); 
+        return res;
+
     } catch (e) {
         throw e;
     }
